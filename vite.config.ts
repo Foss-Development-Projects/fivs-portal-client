8ceb228c6e4f3c89f2a24316c8c1deb335c1670b
Shankha Shubhra Chatterjee <shankhashubhrachatterjee0@gmail.com>
2026-02-07 00:21:10 +0530

    feat: change config files

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,8 @@ export default defineConfig(() => {
       host: '0.0.0.0',
       proxy: {
         '/api': {
-          target: 'http://127.0.0.1:54321',
+          // target: 'http://127.0.0.1:54321',
+          target: 'https://fivs-portal-server.vercel.app',
           changeOrigin: true,
         }
       }
